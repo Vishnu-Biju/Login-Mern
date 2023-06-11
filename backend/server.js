8ec2d9d4fb6002ebdbf07f0c2c3226ef7802e78e
vishnu biju <vishnubiju.in@gmail.com>
2023-06-11 13:27:57 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+// server.js
+
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authMiddleware = require('./middleware/auth');
+const authRoutes = require('./routes/auth');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/auth', authRoutes);
+
+// Protected route example
+app.get('/protected', authMiddleware, (req, res) => {
+  res.json({ message: 'Protected route accessed successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
