05c24afb5d6d42566a9364e12dad8ffdf68bc736
vishnu biju <vishnubiju.in@gmail.com>
2023-06-11 13:41:29 +0530

    DB MODEL created

---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+// models/User.js
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  otp: String,
+});
+
+module.exports = mongoose.model('User', userSchema);
